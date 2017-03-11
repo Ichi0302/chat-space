ae4ce59d2dfecb22b4df96882f6f032ec49578b1
Ichiro Iwasaki <09046849632i@gmail.com>
2017-03-11 17:10:51 +0900

    fix devise html

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20170311061758) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
