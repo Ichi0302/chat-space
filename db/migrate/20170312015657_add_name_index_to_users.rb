1a39e25c67e4c93dc4845a8131c2ada19ec5f881
Ichiro Iwasaki <09046849632i@gmail.com>
2017-03-12 12:12:22 +0900

    Edit registrations

---
+++ db/migrate/20170312015657_add_name_index_to_users.rb
@@ -0,0 +1,6 @@
+class AddNameIndexToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_index :users, :name, unique: true
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311061758) do
+ActiveRecord::Schema.define(version: 20170312015657) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "name"
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170311061758) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
