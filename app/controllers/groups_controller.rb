f06576d12c432c1b1df27fadc3e4947c46860e2e
Ichiro Iwasaki <09046849632i@gmail.com>
2017-03-30 10:56:08 +0900

    fix_groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,15 +5,22 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
+    @group.users << current_user
+  end
+
+
+  def create
+    group = Group.create(tweet_params)
+    if group.save
+      flash[:notice] = "グループを作成しました。"
+      redirect_to controller: :groups, action: :index
+    else
+      flash[:alert] = "グループを作成できませんでした。"
+      redirect_to controller: :group, action: :new
+    end
   end
 
   def edit
-
-  end
-
-  def create
-    Group.create(tweet_params)
-    redirect_to controller: :groups, action: :index
   end
 
   private
