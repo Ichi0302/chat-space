cfdac0886ecf01da1caa8cb7170ea3bd22c3a3aa
Ichiro Iwasaki <09046849632i@gmail.com>
2017-04-03 22:48:22 +0900

    fix_view_files

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,6 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
-    @group_users = @group.users
+    @users = @group.users
   end
 end
