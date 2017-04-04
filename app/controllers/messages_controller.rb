8f206c2eb896c8104fda49a58987922ea66d9e12
Ichiro Iwasaki <09046849632i@gmail.com>
2017-04-04 13:29:58 +0900

    Create_message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,15 @@ class MessagesController < ApplicationController
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
     @users = @group.users
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
