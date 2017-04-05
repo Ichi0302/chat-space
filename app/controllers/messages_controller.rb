d01a4b0835ea7215ce316d3fda803e49456e4746
Ichiro Iwasaki <09046849632i@gmail.com>
2017-04-05 16:20:53 +0900

    fix_messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :set_view_content, only: [:index, :create]
+
   def index
-    @groups = current_user.groups
-    @group = Group.find(params[:group_id])
-    @users = @group.users
-    @messages = @group.messages
     @message = Message.new
   end
 
   def create
-    @message = current_user.messages.create(message_params)
-    unless @message.save
-      redirect_to group_messages_path, flash: {alert: "メッセージを入力してください。"}
-    else
+    @message = current_user.messages.new(message_params)
+    if @message.save
       redirect_to group_messages_path
+    else
+      flash[:alert] = "メッセージを入力してください。"
+      render :index
     end
   end
 
   private
   def message_params
     params.require(:message).permit(:text).merge(group_id: params[:group_id])
+  end
+
+  def set_view_content
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @users = @group.users
+    @messages = @group.messages
   end
 end
