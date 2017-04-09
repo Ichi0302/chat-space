bbf6c1e095e647d6f832fb85c28d7f4e831e1bf3
Ichiro Iwasaki <09046849632i@gmail.com>
2017-04-09 23:37:35 +0900

    Create_user_model_text

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,50 @@
+#nameが入っているか
+#emailが入っているか
+#passwordが入っているか
+#password_confirmationが入っているか
+#emailが重複していないか
+
+#重複、全部入っているか
+
+require 'rails_helper'
+
+describe User do
+  describe "#create" do
+
+    it "is vaild with a name, email, password, and password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "is invalid without a name" do
+      user = build(:user, name: "")
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください。")
+    end
+
+    it "is invalid without a email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください。")
+    end
+
+    it "is invalid without a password" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください。")
+    end
+
+    it "is invalid without a password_confirmation" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません。")
+    end
+
+    it "is invalid with a duplicate email" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します。")
+    end
+  end
+end
