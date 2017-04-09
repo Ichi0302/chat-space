b4ab0a0f48644eeadadc619a6895c8eb663c09e9
Ichiro Iwasaki <09046849632i@gmail.com>
2017-04-09 15:07:53 +0900

    introduce_faker

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
   factory :message do
-    text          "aaaaaaaaaaa"
-    image         "hoge.png"
-    user_id       "1"
-    group_id      "1"
+    text          {Faker::Lorem.sentence}
+    image         {Faker::Avatar.image}
+    user_id       {Faker::Number.number(1)}
+    group_id      {Faker::Avatar.image}
   end
 
 end
