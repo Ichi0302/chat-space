2ebed2396d83fb3ffa3749494cf0e88331399ffc
Ichiro Iwasaki <09046849632i@gmail.com>
2017-04-25 15:03:56 +0900

    fix_message.js3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -7,34 +7,33 @@ $(function() {
   $('#new_message').on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData($(this)[0]);
-    console.log(formData);
-    if(formData != ""){
-      $.ajax({
-        url: './messages',
-        type: 'POST',
-        data: formData,
-        processData: false,
-        contentType: false,
-        dataType: 'json'
+
+    $.ajax({
+      url: './messages',
+      type: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.content-right__chatsholder--chatspace').prepend(html.append(
+           `<div class="username">
+              ${data.name}
+            </div>
+            <div class="chatdate">
+              ${data.created_at}
+            </div>
+            <div class="chattext">
+              ${data.text}
+            </div>`
+          ));
+      $('.content-right__bottom-bar--textplace').val('');
       })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.content-right__chatsholder--chatspace').prepend(html.append(
-             `<div class="username">
-                ${data.name}
-              </div>
-              <div class="chatdate">
-                ${data.created_at}
-              </div>
-              <div class="chattext">
-                ${data.text}
-              </div>`
-            ));
-        $('.content-right__bottom-bar--textplace').val('');
-      });
-    } else {
+    .fail (function(data){
       alert('メッセージを入力してください。');
-    };
+    });
     return false;
   });
 });
