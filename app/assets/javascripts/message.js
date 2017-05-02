a7966b6391f68c55d91289c212c943d3506bbb77
Ichiro Iwasaki <09046849632i@gmail.com>
2017-05-02 19:16:44 +0900

    fix_messages.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function() {
   function buildHTML(message){
-    var messageImage = message.image ? `<img src="${message.image}" alt="写真">` : ``;
+    var messageImage = message.image ? `<img src="${message.image}" alt="${message.image}">` : ``;
     var html = $('<li class="content-right__chatsholder--chatspace--onechat">').append(
            `<div class="username">
               ${message.name}
